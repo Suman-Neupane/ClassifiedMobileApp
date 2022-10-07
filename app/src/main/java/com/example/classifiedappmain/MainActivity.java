f11a3a517e6cfd8512eefae9fc3cc588441ad2e7
Nischal Basyal <nischalbasyal2003@gmail.com>
2022-10-07 10:13:13 +0530

    First Commit

---
+++ app/src/main/java/com/example/classifiedappmain/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.classifiedappmain;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        toolbar = findViewById(R.id.)
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.menu_Open,R.string.close_menu);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+                    case R.id.nav_home:
+                        Log.i("MENU_DRAWER_TAG","HOME item is clicked");
+                        setContentView(R.layout.activity_main);
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                    case R.id.nav_post_business:
+                        Log.i("MENU_DRAWER_TAG","Post Business Item is Clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+
+                    case R.id.nav_categories:
+                        Log.i("MENU_DRAWER_TAG","Post Business Item is Clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+
+
+                }
+
+                return true;
+            }
+        });
+    }
+}
