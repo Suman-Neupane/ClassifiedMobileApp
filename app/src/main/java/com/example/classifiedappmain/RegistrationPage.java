8a1969663ec1baa1ca6d3a835aa2783bd6d85300
Nishchal Basyal <60071083+ainishchal@users.noreply.github.com>
2022-10-07 18:18:19 +0530

    Signup Added

--- app/src/main/java/com/example/classifiedappmain/RegistrationPage.java
+++ app/src/main/java/com/example/classifiedappmain/RegistrationPage.java
@@ -10,5 +10,6 @@ public class RegistrationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
     }
 }
--- app/src/main/java/com/example/classifiedappmain/login.java
+++ app/src/main/java/com/example/classifiedappmain/login.java
@@ -12,6 +12,8 @@ import com.google.android.material.button.MaterialButton;
 public class login extends AppCompatActivity {
     MaterialButton material_login;
     TextView textLogin;
+    TextView textSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,7 @@ public class login extends AppCompatActivity {
 
         material_login=findViewById(R.id.material_login);
         textLogin=findViewById(R.id.textSignUp);
+        textSignup=findViewById(R.id.textSignUp);
         material_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,12 +30,15 @@ public class login extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        textLogin.setOnClickListener(new View.OnClickListener() {
+
+        textSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(login.this,RegistrationPage.class);
                 startActivity(i);
             }
         });
+
+
     }
 }
